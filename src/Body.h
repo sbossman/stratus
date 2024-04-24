997aaa4dcb4700be806a1e5008e2b052d13ad11b
sbossman <bossmans@ufl.edu>
2024-04-24 16:58:13 -0400

    Added future functions to Body class

--- src/Body.h
+++ src/Body.h
@@ -1,12 +1,26 @@
 //
 // Created by summe on 4/24/2024.
 //
+#include <vector>
+#include <iostream>
+#include <fstream>
 
 #ifndef STRATUS_BODY_H
 #define STRATUS_BODY_H
 
+using namespace std;
 
 class Body {
+    vector<string> TEMPLATE_BEGIN;
+    vector<string> TEMPLATE_END;
+    vector<string> temp;
+
+public:
+    Body();
+
+    bool readFromFile(ifstream& file);
+
+    bool writeToFile(ofstream& file);
 
 };
 
