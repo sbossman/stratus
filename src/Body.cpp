b87daeafeee7022d1233c55a6cad6d1ce109bf9d
sbossman <bossmans@ufl.edu>
2024-04-24 17:07:46 -0400

    Added in code for Body.cpp

--- src/Body.cpp
+++ src/Body.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "Body.h"
+
+using namespace std;
+
+Body::Body(){
+   TEMPLATE_BEGIN.push_back("<body>");
+
+   TEMPLATE_END.push_back("</body>");
+   TEMPLATE_END.push_back("</html>");
+}
+
+bool Body::readFromFile(std::ifstream &file) {
+    return true;
+}
+
+bool Body::writeToFile(std::ofstream &file) {
+    for(int i = 0; i < TEMPLATE_BEGIN.size(); i++){
+        file << TEMPLATE_BEGIN[i] << endl;
+    }
+    for(int i = 0; i < TEMPLATE_END.size(); i++){
+        file << TEMPLATE_END[i] << endl;
+    }
+    return true;
+}
